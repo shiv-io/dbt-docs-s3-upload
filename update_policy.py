17b629420fd2d0c233926e1825f86b425dea1ddf
Shiv Gupta <18354771+shiv-io@users.noreply.github.com>
2022-11-17 15:24:56 -0800

    Move print statement

--- update_policy.py
+++ update_policy.py
@@ -7,9 +7,8 @@ print("Policy for bucket:", bucket)
 with open('policy.json', 'r') as f:
     policy = json.load(f)
 
-print(policy)
-
 policy['Statement'][0]['Resource'] = [f'arn:aws:s3:::{bucket}/*']
+print(policy)
 
 with open('policy_updated.json', 'w') as f:
     json.dump(policy, f, indent=4)
